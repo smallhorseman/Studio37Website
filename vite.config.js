1cf28247a440a4f25a73c5abcaf3c9d110211d24
Studio 37 <ceo@ponyboy.win>
2025-08-24 12:27:00 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // The resolve alias has been removed to fix the pathing issue.
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 })
