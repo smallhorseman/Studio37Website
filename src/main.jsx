a71b719c95567665c1d8541ad3202ff2962fc8af
Studio 37 <ceo@ponyboy.win>
2025-08-24 09:05:38 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import './style.css' // <-- This is the corrected line
+import './index.css' // This should be the only CSS import
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
