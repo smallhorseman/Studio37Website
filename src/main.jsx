8891ae7fe8b09e0ab2ebee766de5a92e701c87b5
Studio 37 <ceo@ponyboy.win>
2025-08-29 02:04:47 -0500

    Rename App to AppWrapper in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // This should be the only CSS import
+import AppWrapper from './App.jsx' // Note the change here
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWrapper />
   </React.StrictMode>,
 )
