81b95e1f3a41a117a9ec589a10e0e5f6063e987c
Ponyboy <ceo@ponyboy.win>
2025-09-02 18:53:31 -0500

    d

--- packages/web/postcss.config.js
+++ packages/web/postcss.config.js
@@ -1,22 +1,17 @@
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-
-// Removed duplicate export default block to fix multiple default exports error.
-
-const postcssImport = safeRequire('postcss-import');
-const nesting = safeRequire('postcss-nesting');
-const cssnano = isProd
-  ? safeRequire('cssnano')?.({
-      preset: ['default', { discardComments: { removeAll: true } }],
-    })
-  : null;
+// If you use postcss-import, postcss-nesting, cssnano, import them here:
+// import postcssImport from 'postcss-import';
+// import nesting from 'postcss-nesting';
+// import cssnano from 'cssnano';
 
 export default {
   plugins: [
-    postcssImport && postcssImport(),
-    nesting && nesting(),
+    // Uncomment if you use these plugins:
+    // postcssImport && postcssImport(),
+    // nesting && nesting(),
     tailwindcss(),
     autoprefixer(),
-    cssnano,
+    // cssnano,
   ].filter(Boolean),
 };
