93f244823afc92f8b31c68b0841a1d56dfc49093
Ponyboy <ceo@ponyboy.win>
2025-09-02 04:17:14 -0500

    g

--- packages/web/src/main.jsx
+++ packages/web/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './AuthContext.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
