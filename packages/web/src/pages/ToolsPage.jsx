eed8f7b80c460a3cc5c2373fead35cf4c0d5d5e5
Ponyboy <ceo@ponyboy.win>
2025-09-02 13:44:03 -0500

    x

---
+++ packages/web/src/pages/ToolsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function ToolsPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-handwriting mb-6">Admin Tools</h1>
+      <p>This is the tools page for admin users.</p>
+    </div>
+  );
+}
