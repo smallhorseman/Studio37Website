98098b9ea5bbd59009bd7727948ae8f8e7add708
Studio 37 <ceo@ponyboy.win>
2025-09-03 21:12:02 -0500

    s

--- packages/web/src/components/ProtectedRoute.jsx
+++ packages/web/src/components/ProtectedRoute.jsx
@@ -7,14 +7,10 @@ export default function ProtectedRoute({ children }) {
   const location = useLocation();
 
   if (!isReady) {
-    // You can return a loading spinner here if you want
     return <div className="p-8 text-center">Authenticating...</div>;
   }
 
   if (!isAuthenticated) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to. This allows us to send them along to that page after they
-    // log in, which is a nicer user experience.
     return <Navigate to={`/login?next=${location.pathname}`} replace />;
   }
 
