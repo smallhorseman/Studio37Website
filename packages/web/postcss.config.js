7a6d29eb19f94247cee4caacbcc3e8ebaf33ed80
Studio 37 <ceo@ponyboy.win>
2025-09-03 23:08:23 -0500

    fr

--- packages/web/postcss.config.js
+++ packages/web/postcss.config.js
@@ -1,8 +1,2 @@
-// Tailwind 4 requires a different PostCSS setup
-// Removing all plugins to let Vite handle it
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
+// Completely remove PostCSS configuration - let Vite handle it internally
+export default {}
