1f429c373903ed84484e2f8829418fef01edf871
Studio 37 <ceo@ponyboy.win>
2025-09-03 19:20:24 -0500

    y

--- packages/web/eslint.config.js
+++ packages/web/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tailwindcss from '@tailwindcss/eslint-plugin'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -13,6 +14,7 @@ export default defineConfig([
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       '@tailwindcss': tailwindcss,
+      'jsx-a11y': jsxA11y,
     },
     languageOptions: {
       ecmaVersion: 2020,
@@ -25,13 +27,10 @@ export default defineConfig([
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
       ...tailwindcss.configs.recommended,
       'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': 'warn',
-    },
-  },
-])
-      'tailwindcss/no-custom-classname': 'warn',
     },
   },
 ])
